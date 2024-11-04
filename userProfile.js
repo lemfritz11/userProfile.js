8a7f0ce1cccc5439ee9c8ff463da1396183b2cab
lemfritz11 <aintdaiki@gmail.com>
2024-11-04 19:07:09 +0800

    initial commit with userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Lem Fritz Refuerzo";
+let age = 20;
+let favoriteNumber = 11;
+let favoriteColor = "Black";
+
+console.log("Full Name: " + fullName);
+console.log("Age: " + age);
+console.log("Favorite Number: " + favoriteNumber);
+console.log("Favorite Color: " + favoriteColor);
